89b322e145e2166e4515e571db17296c4f8576e0
PauVicino <47858768+PauVicino@users.noreply.github.com>
2020-12-03 16:52:48 -0500

    Add files via upload

--- contactForm.php
+++ contactForm.php
@@ -5,10 +5,12 @@ include('database_conn.php');
 session_start();
 
 $data = array(
-      ':to_user_id' => $_POST['to_user_id'],
+      ':to_user_id' => $_POST['reciever_id'],
       ':from_user_id' => $_SESSION['user_id'],
       ':message' => $_POST['message']
 );
+
+// updates the mysql message database every time a message is sent
 
 $sql = "INSERT INTO message (message, from_user_id, to_user_id) VALUES (:message, :from_user_id, :to_user_id)";
 
@@ -16,7 +18,7 @@ $stmt = $conn->prepare($sql);
 
 if ($stmt->execute($data))
 {
-      echo fetch_user_chat_history($_SESSION['user_id'], $_POST['to_user_id'], $conn);
+      echo fetch_user_chat_history($_SESSION['user_id'], $_POST['reciever_id'], $conn);
 }
 
  ?>

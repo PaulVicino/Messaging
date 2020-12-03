141283155fd41a0fca3a27743f4d5b11fefc4d35
PauVicino <47858768+PauVicino@users.noreply.github.com>
2020-12-03 16:53:29 -0500

    Add files via upload

--- contactForm.php
+++ contactForm.php
@@ -5,7 +5,7 @@ include('database_conn.php');
 session_start();
 
 $data = array(
-      ':to_user_id' => $_POST['to_user_id'],
+      ':to_user_id' => $_POST['reciever_id'],
       ':from_user_id' => $_SESSION['user_id'],
       ':message' => $_POST['message']
 );
@@ -18,7 +18,7 @@ $stmt = $conn->prepare($sql);
 
 if ($stmt->execute($data))
 {
-      echo fetch_user_chat_history($_SESSION['user_id'], $_POST['to_user_id'], $conn);
+      echo fetch_user_chat_history($_SESSION['user_id'], $_POST['reciever_id'], $conn);
 }
 
  ?>
